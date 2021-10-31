303c44c8bc558ea99d8510f43f32eb0b9e3df76f
ghchoi0427 <ghchoi0427@gmail.com>
2021-10-31 20:17:09 +0900

    feat: userType에 맞게 액티비티 전환

--- app/src/main/java/com/choi/cafe/ui/login/LoginActivity.java
+++ app/src/main/java/com/choi/cafe/ui/login/LoginActivity.java
@@ -1,30 +1,24 @@
 package com.choi.cafe.ui.login;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.choi.cafe.CustomerActivity;
 import com.choi.cafe.R;
-import com.choi.cafe.ui.login.LoginViewModel;
-import com.choi.cafe.ui.login.LoginViewModelFactory;
+import com.choi.cafe.StaffActivity;
 import com.choi.cafe.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
@@ -115,6 +109,12 @@ public class LoginActivity extends AppCompatActivity {
         String welcome = getString(R.string.welcome) + model.getDisplayName();
         // TODO : initiate successful logged in experience
         Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+        if (model.getUserType().equals(UserType.Staff)) {
+            startActivity(new Intent(this, StaffActivity.class));
+        }
+        if (model.getUserType().equals(UserType.Customer)) {
+            startActivity(new Intent(this, CustomerActivity.class));
+        }
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
