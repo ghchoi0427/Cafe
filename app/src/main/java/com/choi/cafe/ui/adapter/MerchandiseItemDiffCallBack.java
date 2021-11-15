22ac12b32192fe6fa9ec4c778b09da2a1fcca0c1
ghchoi0427 <ghchoi0427@gmail.com>
2021-11-15 16:36:32 +0900

    feat: MerchandiseItemDiffCallBack 생성

---
+++ app/src/main/java/com/choi/cafe/ui/adapter/MerchandiseItemDiffCallBack.java
@@ -0,0 +1,50 @@
+package com.choi.cafe.ui.adapter;
+
+import androidx.recyclerview.widget.DiffUtil;
+
+import com.choi.cafe.data.model.Merchandise;
+
+import java.util.ArrayList;
+
+public class MerchandiseItemDiffCallBack extends DiffUtil.Callback {
+    private ArrayList<Merchandise> oldMerchandiseList;
+    private ArrayList<Merchandise> newMerchandiseList;
+
+    public MerchandiseItemDiffCallBack(ArrayList<Merchandise> oldMerchandiseList, ArrayList<Merchandise> newMerchandiseList) {
+        this.oldMerchandiseList = oldMerchandiseList;
+        this.newMerchandiseList = newMerchandiseList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldMerchandiseList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newMerchandiseList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        Merchandise oldItem = oldMerchandiseList.get(oldItemPosition);
+        Merchandise newItem = newMerchandiseList.get(newItemPosition);
+
+        return oldItem.equals(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        Merchandise oldItem = oldMerchandiseList.get(oldItemPosition);
+        Merchandise newItem = newMerchandiseList.get(newItemPosition);
+
+        if (!oldItem.getName().equals(newItem.getName()))
+            return false;
+        if (!(oldItem.getPrice() == newItem.getPrice()))
+            return false;
+        if (!oldItem.getSerialNumber().equals(newItem.getSerialNumber()))
+            return false;
+
+        return true;
+    }
+}
