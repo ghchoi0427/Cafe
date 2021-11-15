920a123a4f4d5221ab088003d9d572e32db64264
ghchoi0427 <ghchoi0427@gmail.com>
2021-11-15 16:39:23 +0900

    fix: setItems 메서드 추가

--- app/src/main/java/com/choi/cafe/ui/merchandise/MerchandiseFragment.java
+++ app/src/main/java/com/choi/cafe/ui/merchandise/MerchandiseFragment.java
@@ -7,32 +7,57 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.choi.cafe.data.model.Merchandise;
 import com.choi.cafe.databinding.FragmentMerchandiseBinding;
+import com.choi.cafe.ui.adapter.MerchandiseAdapter;
 
 public class MerchandiseFragment extends Fragment {
+
+    public MerchandiseViewModel getMerchandiseViewModel() {
+        return merchandiseViewModel;
+    }
 
     private com.choi.cafe.ui.merchandise.MerchandiseViewModel merchandiseViewModel;
     private FragmentMerchandiseBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        merchandiseViewModel =
-                new ViewModelProvider(this).get(com.choi.cafe.ui.merchandise.MerchandiseViewModel.class);
+        merchandiseViewModel = new ViewModelProvider(this).get(com.choi.cafe.ui.merchandise.MerchandiseViewModel.class);
 
         binding = FragmentMerchandiseBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
         final TextView textView = binding.textMerchandise;
+        final RecyclerView recyclerMerchandise = binding.recyclerMerchandise;
+        recyclerMerchandise.setLayoutManager(new LinearLayoutManager(getContext()));
+        MerchandiseAdapter merchandiseAdapter = new MerchandiseAdapter();
+        recyclerMerchandise.setAdapter(merchandiseAdapter);
         merchandiseViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+
+
+        setItems(recyclerMerchandise, merchandiseViewModel.getMerchandiseList());
+        return binding.getRoot();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public static void setItems(RecyclerView recyclerView, ObservableArrayList<Merchandise> merchandiseList) {
+        MerchandiseAdapter adapter;
+        if (recyclerView.getAdapter() == null) {
+            adapter = new MerchandiseAdapter();
+            recyclerView.setAdapter(adapter);
+        } else {
+            adapter = (MerchandiseAdapter) recyclerView.getAdapter();
+        }
+        adapter.updateItems(merchandiseList);
     }
 }
