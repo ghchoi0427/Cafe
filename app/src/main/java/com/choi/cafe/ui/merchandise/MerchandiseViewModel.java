0a6b8f1b9ee8208fe8948ccc64da6e603a677071
ghchoi0427 <ghchoi0427@gmail.com>
2021-11-23 21:54:01 +0900

    refactor: 필요없는 메서드 삭제

--- app/src/main/java/com/choi/cafe/ui/merchandise/MerchandiseViewModel.java
+++ app/src/main/java/com/choi/cafe/ui/merchandise/MerchandiseViewModel.java
@@ -1,20 +1,15 @@
 package com.choi.cafe.ui.merchandise;
 
 import androidx.databinding.ObservableArrayList;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.choi.cafe.data.model.Merchandise;
 
 public class MerchandiseViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
     private ObservableArrayList<Merchandise> merchandises;
 
     public MerchandiseViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
         merchandises = new ObservableArrayList<>();
         setMerchandiseList(merchandises);
     }
@@ -32,7 +27,4 @@ public class MerchandiseViewModel extends ViewModel {
         this.merchandises = merchandises;
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
