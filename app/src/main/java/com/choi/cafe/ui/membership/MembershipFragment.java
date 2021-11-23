973ecb85a5fcd495bf2d291d68ae39dd3002a2dd
ghchoi0427 <ghchoi0427@gmail.com>
2021-11-23 21:48:52 +0900

    feat: setItems 생성

--- app/src/main/java/com/choi/cafe/ui/membership/MembershipFragment.java
+++ app/src/main/java/com/choi/cafe/ui/membership/MembershipFragment.java
@@ -4,20 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.choi.cafe.R;
+import com.choi.cafe.data.model.Membership;
 import com.choi.cafe.databinding.FragmentMembershipBinding;
+import com.choi.cafe.firebase.FireStore;
+import com.choi.cafe.ui.adapter.membership.MembershipAdapter;
 
 public class MembershipFragment extends Fragment {
 
     private MembershipViewModel membershipViewModel;
     private FragmentMembershipBinding fragmentMembershipBinding;
+    FireStore fireStore;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,8 +32,15 @@ public class MembershipFragment extends Fragment {
         fragmentMembershipBinding = FragmentMembershipBinding.inflate(inflater, container, false);
         View root = fragmentMembershipBinding.getRoot();
 
-        final TextView textView = fragmentMembershipBinding.textMembership;
-        membershipViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final RecyclerView recyclerMembership = fragmentMembershipBinding.recyclerMembership;
+        recyclerMembership.setLayoutManager(new LinearLayoutManager(getContext()));
+        MembershipAdapter membershipAdapter = new MembershipAdapter();
+        recyclerMembership.setAdapter(membershipAdapter);
+        setItems(recyclerMembership, membershipViewModel.getMembershipList());
+
+        fireStore = new FireStore();
+        fireStore.getAllData(getString(R.string.collection_membership), recyclerMembership, this);
+
         return root;
     }
 
@@ -36,5 +48,16 @@ public class MembershipFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         fragmentMembershipBinding = null;
+    }
+
+    public static void setItems(RecyclerView recyclerView, ObservableArrayList<Membership> membershipList) {
+        MembershipAdapter adapter;
+        if (recyclerView.getAdapter() == null) {
+            adapter = new MembershipAdapter();
+            recyclerView.setAdapter(adapter);
+        } else {
+            adapter = (MembershipAdapter) recyclerView.getAdapter();
+        }
+        adapter.updateItems(membershipList);
     }
 }
