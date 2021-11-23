dda5aa753b47c75233e66cac52500d465faca7c9
ghchoi0427 <ghchoi0427@gmail.com>
2021-11-23 21:46:21 +0900

    fix: 호출한 fragment에 따라 다른 컬렉션에서 데이터 불러오기

--- app/src/main/java/com/choi/cafe/firebase/FireStore.java
+++ app/src/main/java/com/choi/cafe/firebase/FireStore.java
@@ -5,7 +5,9 @@ import android.util.Log;
 import androidx.databinding.ObservableArrayList;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.choi.cafe.data.model.Membership;
 import com.choi.cafe.data.model.Merchandise;
+import com.choi.cafe.ui.membership.MembershipFragment;
 import com.choi.cafe.ui.merchandise.MerchandiseFragment;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -38,22 +40,41 @@ public class FireStore {
         });
     }
 
-    public void getAllData(String collectionPath, RecyclerView recyclerView) {
-        db.collection(collectionPath)
-                .get()
-                .addOnCompleteListener(task -> {
-                    if (task.isSuccessful()) {
-                        ObservableArrayList<Merchandise> list = new ObservableArrayList<>();
-                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
-                            Merchandise merchandise = document.toObject(Merchandise.class);
-                            list.add(merchandise);
+    public void getAllData(String collectionPath, RecyclerView recyclerView, Object object) {
+        //TODO: generify this method
+        if (object.getClass().equals(MerchandiseFragment.class)) {
+            db.collection(collectionPath)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            ObservableArrayList<Merchandise> list = new ObservableArrayList<>();
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                Merchandise merchandise = document.toObject(Merchandise.class);
+                                list.add(merchandise);
+                            }
+                            MerchandiseFragment.setItems(recyclerView, list);
+                        } else {
+                            Log.d("tester", "Error getting documents: ", task.getException());
                         }
-                        MerchandiseFragment.setItems(recyclerView, list);
-                    } else {
-                        Log.d("tester", "Error getting documents: ", task.getException());
-                    }
-                });
+                    });
 
+        }
+        if (object.getClass().equals(MembershipFragment.class)) {
+            db.collection(collectionPath)
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            ObservableArrayList<Membership> list = new ObservableArrayList<>();
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                Membership membership = document.toObject(Membership.class);
+                                list.add(membership);
+                            }
+                            MembershipFragment.setItems(recyclerView, list);
+                        } else {
+                            Log.d("tester", "Error getting documents: ", task.getException());
+                        }
+                    });
+        }
     }
 
 }
