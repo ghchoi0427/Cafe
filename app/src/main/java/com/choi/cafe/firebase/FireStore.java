f770dca841649b0a8a5cfec5c30bbc94aa1c6e17
ghchoi0427 <ghchoi0427@gmail.com>
2021-12-15 23:25:35 +0900

    feat: getStaffData 생성

--- app/src/main/java/com/choi/cafe/firebase/FireStore.java
+++ app/src/main/java/com/choi/cafe/firebase/FireStore.java
@@ -1,12 +1,14 @@
 package com.choi.cafe.firebase;
 
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.databinding.ObservableArrayList;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.choi.cafe.data.model.Membership;
 import com.choi.cafe.data.model.Merchandise;
+import com.choi.cafe.data.model.Staff;
 import com.choi.cafe.ui.membership.MembershipFragment;
 import com.choi.cafe.ui.merchandise.MerchandiseFragment;
 import com.google.firebase.firestore.DocumentReference;
@@ -38,6 +40,26 @@ public class FireStore {
                 Log.d("TAG", "get failed with ", task.getException());
             }
         });
+    }
+
+    public void getStaffData(String collectionPath, String key, String value, TextView[] textViews) {
+
+        db.collection(collectionPath)
+                .whereEqualTo(key, value)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                            Staff staff = document.toObject(Staff.class);
+                            textViews[0].setText("이름:" + staff.getName());
+                            textViews[1].setText("나이:" +staff.getAge());
+                            textViews[2].setText("성별:" +staff.getSex());
+                            textViews[3].setText("직급:" +staff.getPosition());
+                        }
+                    } else {
+
+                    }
+                });
     }
 
     public void getAllData(String collectionPath, RecyclerView recyclerView, Object object) {
